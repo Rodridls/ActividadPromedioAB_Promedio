0f524e9c1f1864386c7043f041569dd7e45632a8
Rodridls <126906061+Rodridls@users.noreply.github.com>
2023-06-08 20:27:41 -0300

    Add files via upload
    Terminado el proyecto.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promedio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Promedio promedio = new Promedio();
+            Console.WriteLine("1 - Agregar valor \t\n2 - Calcular Promedio \t\n3 - Salir");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                switch (opcion)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Ingrese un valor");
+                        double valor = Convert.ToDouble(Console.ReadLine());
+                        promedio.agregarValor(valor);
+                        break;
+                    case 2:
+                        double prom = promedio.calcularPromedio();
+                        Console.Clear();
+                        Console.WriteLine("Su promedio es de: {0:F2}" , prom);
+                        Console.ReadKey(); 
+                        break;
+                    case 3:
+                        break;
+                }
+                Console.Clear();    
+                Console.WriteLine("1 - Agregar valor \t\n2 - Calcular Promedio \t\n3 - Salir");
+                opcion = Convert.ToInt32(Console.ReadLine());
+            } while (opcion != 3) ;
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
---
+++ Promedio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promedio
+{
+    internal class Promedio
+    {
+        int contador = 0;
+        double acumular = 0;
+
+        public void agregarValor(double valor)
+        {
+            contador++;
+            acumular += valor;
+        }
+        public double calcularPromedio() 
+        {
+            if (contador != 0) 
+            {
+            return acumular/ contador;
+            }
+            return 0;
+        }
+    }
+}
